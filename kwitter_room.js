65b777cd3c20f9c47b2fe9ba10ab83fe6e841635
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2022-04-02 20:47:10 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+
+// Your web app's Firebase configuration
+
+
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
